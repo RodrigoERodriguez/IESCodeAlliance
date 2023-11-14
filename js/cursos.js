33ba5cb72b8e8cd5cf36bfbeaae908e00cae2b70
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2023-11-14 04:52:46 -0300

    Se agrego renderizado, resta finalizar render documentacion, y rerenderizado por seleccion

--- js/cursos.js
+++ js/cursos.js
@@ -1,15 +1,16 @@
 //---------------------------------------------------------------//
 
-let arrayCursos = [];
+let cursos = [];
 
 //---------------------------------------------------------------//
 
 fetch("../data/cursos.json")
     .then(response => response.json())
     .then(data => {
-        arrayCursos = data;
-        cargarCursos(arrayCursos);
+        cursos = data;
+        cargarCursos(cursos);
     })
+
 //---------------------------------------------------------------//
 
 //- ELEMENTOS DEL DOM -//
@@ -18,27 +19,27 @@ const contenedorCursos = document.querySelector("#curso__grupo-contenedor")
 //---------------------------------------------------------------//
 
 function cargarCursos() {
-
     contenedorCursos.innerHTML = "";
 
-    arrayCursos.forEach(curso => {
-        
+    cursos.curso.forEach(curso => {
+
         const div = document.createElement("div");
         div.classList.add("curso");
+        
         div.innerHTML = `
-            <h2 class="c__titulo-categoria">${curso.curso.nombre}</h2>
+            <h2 class="c__titulo-categoria">${curso.nombre}</h2>
             <div class="c__contenedor-videos-categoria">
-                <div class="c__video-categoria">
-                    <img src="${curso.videos.miniatura}" alt="${curso.videos.titulo}">
-                    <h3>${curso.videos.titulo}</h3>
-                    <h3>${curso.videos.autor}</h3>
-                    <h3>${curso.videos.duracion}</h3>
-                </div>
+                ${curso.videos.map(video => `
+                    <div class="c__video-categoria">
+                        <img id="catalogo__imagen-curso" src="${video.miniatura}" alt="${video.titulo}">
+                        <h3>${video.titulo}</h3>
+                        <h3>${video.autor}</h3>
+                        <h3>${video.duracion}</h3>
+                    </div>
+                `).join('')}
             </div>
         `;
 
         contenedorCursos.appendChild(div);
     });
 }
-
-cargarCursos();
