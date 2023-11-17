7e87572bd877c5442f3b726e00f97390d8d28352
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2023-11-17 17:50:32 -0300

    Corregido JSON novedades, se agrega funcion filtrado(Corregir), cambios en TITLE en los documentos html, resta configurar DOCUMENTACION.HTML

--- js/cursos.js
+++ js/cursos.js
@@ -1,51 +1,73 @@
-//---------------------------------------------------------------//
+document.addEventListener('DOMContentLoaded', function () {
 
-let cursos = [];
+    //---------------------------------------------------------------//
 
-//---------------------------------------------------------------//
+    let cursos = [];
 
-fetch("../data/cursos.json")
+    //---------------------------------------------------------------//
+
+    fetch("../data/cursos.json")
     .then(response => response.json())
     .then(data => {
         cursos = data;
         cargarCursos(cursos);
-    })
-
-//---------------------------------------------------------------//
-
-//- ELEMENTOS DEL DOM -//
-const contenedorCursos = document.querySelector("#cursos__grupo-contenedor")
-const eleccionLenguaje = document.getElementById("cursos__lenguajes-filtro");
-const botonFiltrado = document.getElementById("cursos__boton-filtrar");
-
-//---------------------------------------------------------------//
-
-function cargarCursos() {
-    contenedorCursos.innerHTML = "";
-
-    cursos.curso.forEach(curso => {
-
-        const div = document.createElement("div");
-        div.classList.add("curso");
-        
-        div.innerHTML = `
-            <h2 class="cursos__titulo-categoria">${curso.nombre}</h2>
-            <div class="cursos__contenedor-videos-categoria">
-                ${curso.videos.map(video => `
-                    <div class="cursos__video-categoria">
-                        <img id="cursos__imagen-curso" src="${video.miniatura}" alt="${video.titulo}">
-                        <h3 class="cursos__texto-titulo">${video.titulo}</h3>
-                        <h3 class="cursos__texto-autor">${video.autor}</h3>
-                        <h3 class="cursos__texto-duracion">${video.duracion}</h3>
-                    </div>
-                `).join('')}
-            </div>
-        `;
-
-        contenedorCursos.appendChild(div);
     });
-}
+
+    //---------------------------------------------------------------//
+
+    //- ELEMENTOS DEL DOM -//
+
+    const contenedorCursos = document.querySelector("#cursos__grupo-contenedor");
+    const eleccionLenguaje = document.getElementById("cursos__lenguajes-filtro");
+    const botonFiltrado = document.getElementById("cursos__boton-filtrar");
 
 
-//- Eventos -//
+    //---------------------------------------------------------------//
 
+    botonFiltrado.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const categoriaSeleccionada = eleccionLenguaje.elements['categoria'].value;
+
+        const cursosFiltrados = filtrarCursos(categoriaSeleccionada, cursos);
+
+        cargarCursos(cursosFiltrados);
+    });
+
+    //---------------------------------------------------------------//
+
+    function cargarCursos(cursos) {
+        contenedorCursos.innerHTML = "";
+
+        cursos.curso.forEach(curso => {
+            const div = document.createElement("div");
+            div.classList.add("curso");
+
+            div.innerHTML = `
+                <h2 class="cursos__titulo-categoria">${curso.nombre}</h2>
+                <div class="cursos__contenedor-videos-categoria">
+                    ${curso.videos.map(video => `
+                        <div class="cursos__video-categoria">
+                            <img id="cursos__imagen-curso" src="${video.miniatura}" alt="${video.titulo}">
+                            <h3 class="cursos__texto-titulo">${video.titulo}</h3>
+                            <h3 class="cursos__texto-autor">${video.autor}</h3>
+                            <h3 class="cursos__texto-duracion">${video.duracion}</h3>
+                        </div>
+                    `).join('')}
+                </div>
+            `;
+
+            contenedorCursos.appendChild(div);
+        });
+    }
+
+    //---------------------------------------------------------------//
+
+    function filtrarCursos(categoria, cursos) {
+        if (categoria === '0') {
+            return cursos;
+        } else {
+            return cursos.curso.filter(curso => curso.nombre.toString() === categoria);
+        }
+    }
+});
