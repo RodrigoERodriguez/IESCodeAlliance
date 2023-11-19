63e718ad0ebc1267d81244639c263688ac9e1ae0
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2023-11-18 23:01:32 -0300

    Correccion de Render inicial

--- js/cursos.js
+++ js/cursos.js
@@ -1,28 +1,54 @@
-document.addEventListener("change", function () {
+//---------------------------------------------------------------//
 
-    //---------------------------------------------------------------//
+let cursos = [];
 
-    let cursos = [];
+//---------------------------------------------------------------//
 
-    //---------------------------------------------------------------//
+fetch("../data/cursos.json")
+.then(response => response.json())
+.then(data => {
+    cursos = data;
+    cargarCursos(cursos);
+});
 
-    fetch("../data/cursos.json")
-    .then(response => response.json())
-    .then(data => {
-        cursos = data;
-        cargarCursos(cursos);
+//---------------------------------------------------------------//
+
+//- ELEMENTOS DEL DOM -//
+
+const contenedorCursos = document.querySelector("#cursos__grupo-contenedor");
+const eleccionLenguaje = document.getElementById("cursos__lenguajes-filtro");
+const botonFiltrado = document.getElementById("cursos__boton-filtrar");
+
+
+//---------------------------------------------------------------//
+
+
+function cargarCursos(cursos) {
+    contenedorCursos.innerHTML = "";
+
+    cursos.curso.forEach(curso => {
+        const div = document.createElement("div");
+        div.classList.add("curso");
+
+        div.innerHTML = `
+            <h2 class="cursos__titulo-categoria">${curso.nombre}</h2>
+            <div class="cursos__contenedor-videos-categoria">
+                ${curso.videos.map(video => `
+                    <div class="cursos__video-categoria">
+                        <img id="cursos__imagen-curso" src="${video.miniatura}" alt="${video.titulo}">
+                        <h3 class="cursos__texto-titulo">${video.titulo}</h3>
+                        <h3 class="cursos__texto-autor">${video.autor}</h3>
+                        <h3 class="cursos__texto-duracion">${video.duracion}</h3>
+                    </div>
+                `).join('')}
+            </div>
+        `;
+
+        contenedorCursos.appendChild(div);
     });
+}
 
-    //---------------------------------------------------------------//
-
-    //- ELEMENTOS DEL DOM -//
-
-    const contenedorCursos = document.querySelector("#cursos__grupo-contenedor");
-    const eleccionLenguaje = document.getElementById("cursos__lenguajes-filtro");
-    const botonFiltrado = document.getElementById("cursos__boton-filtrar");
-
-
-    //---------------------------------------------------------------//
+document.addEventListener("change", function () {
 
     botonFiltrado.addEventListener('click', function (event) {
         event.preventDefault();
@@ -33,33 +59,6 @@ document.addEventListener("change", function () {
 
         cargarCursos(cursosFiltrados);
     });
-
-    //---------------------------------------------------------------//
-
-    function cargarCursos(cursos) {
-        contenedorCursos.innerHTML = "";
-
-        cursos.curso.forEach(curso => {
-            const div = document.createElement("div");
-            div.classList.add("curso");
-
-            div.innerHTML = `
-                <h2 class="cursos__titulo-categoria">${curso.nombre}</h2>
-                <div class="cursos__contenedor-videos-categoria">
-                    ${curso.videos.map(video => `
-                        <div class="cursos__video-categoria">
-                            <img id="cursos__imagen-curso" src="${video.miniatura}" alt="${video.titulo}">
-                            <h3 class="cursos__texto-titulo">${video.titulo}</h3>
-                            <h3 class="cursos__texto-autor">${video.autor}</h3>
-                            <h3 class="cursos__texto-duracion">${video.duracion}</h3>
-                        </div>
-                    `).join('')}
-                </div>
-            `;
-
-            contenedorCursos.appendChild(div);
-        });
-    }
 
     //---------------------------------------------------------------//
 
